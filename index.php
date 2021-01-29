72c6900e01eebd7b93d9d05f3a71c7142d150746
mikeserov <serov.m@yandex.ru>
2021-01-29 11:28:03 +0500

    Написал код подключения к БД и установки кодировки

--- index.php
+++ index.php
@@ -10,4 +10,24 @@ $layout_content = include_template('layout.php', [
     'page_content' => $page_content,
     'title' => $title
 ]);
+
+$link = mysqli_connect("localhost", "root", "root", "yeti_cave");
+if (!$link) {
+	print('Ошибка соединения № ' .mysqli_connect_errno(). ': '.mysqli_connect_error());
+} else {
+	print('Соединение успешно установлено.');
+}
+
+printf("Первоначальная кодировка: %s\n", mysqli_character_set_name($link));
+
+if (! mysqli_set_charset($link, "utf8")) {
+	print('Ошибка № ' .mysqli_errno($link). ': '.mysqli_error($link));
+} else {
+	print('Кодировка успешно установлена. Текущая кодировка: '.mysqli_character_set_name($link));
+}
+
+
+
+/*$stm = 'select * from categories;*/
+
 print($layout_content);
